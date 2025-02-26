b27993f951cdf7734cf031cf24f0257251b308ee
superCoder471 <125621366+superCoder471@users.noreply.github.com>
2025-02-25 19:07:56 -0500

    made search non interactable

--- src/components/EventSearcher.tsx
+++ src/components/EventSearcher.tsx
@@ -106,7 +106,7 @@ export const EventSearcher = () => {
                     Input year, and search for events
                 </Paragraph>
             </div>
-            <div className="relative flex w-full gap-3">
+            <div className={`relative flex w-full gap-3 ${ !connectionState || settings.team == "" ? "pointer-events-none" : "pointer-events-auto"}`}>
                 <Input
                     placeholder="Year"
                     className="bg-neutral-200"
@@ -117,7 +117,7 @@ export const EventSearcher = () => {
                 <span className="absolute -bottom-4 left-1 text-xs">
                     {"Team: " + settings?.team}
                 </span>
-                <Button size="md" variant="secondary" onClick={() => search()}>
+                <Button size="md" variant="secondary" onClick={() => search()} >
                     Search
                 </Button>
             </div>
@@ -139,14 +139,14 @@ export const EventSearcher = () => {
                     connectionState ? "hidden" : ""
                 }`}
             >
-                connect to the internet to use
+                connect to the internet to use the search button
             </span>
             <span
                 className={`absolute -bottom-4 left-0 text-xs text-red-500 ${
                     !connectionState === false && settings.team == "" ? "" : "hidden"
                 }`}
             >
-                please select a team in settings
+                please select a team in settings to continue
             </span>
         </div>
     )
