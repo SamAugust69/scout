b091283793de6fa590c6f6a5c4fde601ec4a17ee
superCoder471 <125621366+superCoder471@users.noreply.github.com>
2025-02-27 19:13:35 -0500

    fixed hanging from cage comments

--- src/components/forms/2025/FormPages2025.tsx
+++ src/components/forms/2025/FormPages2025.tsx
@@ -257,7 +257,7 @@ const Teleop2025 = ({ handleChange, formChanges }: FormPageInterface) => {
                             >
                                 <FormInputTitle>Hung Deep</FormInputTitle>
                                 <FormInputDescription>
-                                    Did your robot park under the barge
+                                    Did your robot hang on the cage close to the ground?
                                 </FormInputDescription>
                             </FormInputToggle>
                         </FormField>
@@ -273,7 +273,7 @@ const Teleop2025 = ({ handleChange, formChanges }: FormPageInterface) => {
                             >
                                 <FormInputTitle>Hung Shallow</FormInputTitle>
                                 <FormInputDescription>
-                                    Did your robot park under the barge
+                                    Did your robot hang on the cage high up?
                                 </FormInputDescription>
                             </FormInputToggle>
                         </FormField>
