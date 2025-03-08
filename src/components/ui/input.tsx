cea2cf76bb6dd850c1597cb4ade61ef77111a720
nutty-155 <technonuts155@gmail.com>
2025-03-08 14:47:26 -0500

    sutff

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,19 +1,18 @@
 import { cn } from "@/lib/utils"
-import { InputHTMLAttributes } from "react"
+import React, { InputHTMLAttributes } from "react"
 
 interface InputInterface {}
 
-export const Input = ({
-    className,
-    ...props
-}: InputHTMLAttributes<HTMLInputElement> & InputInterface) => {
+export const Input = React.forwardRef<HTMLInputElement, InputHTMLAttributes<HTMLInputElement> & InputInterface>(({className, ...props}, ref) => {
     return (
         <input
-            {...props}
-            className={cn(
-                "w-full rounded-sm border-neutral-200 bg-neutral-300 px-4 py-2 placeholder-neutral-500 outline-hidden invalid:border-red-500 focus:ring-2 dark:border-neutral-800 dark:bg-neutral-900 dark:text-neutral-300 dark:placeholder-neutral-400",
-                className
-            )}
-        />
+        {...props}
+        ref={ref}
+        className={cn(
+            "w-full rounded-sm border-neutral-200 bg-neutral-300 px-4 py-2 placeholder-neutral-500 outline-hidden invalid:border-red-500 focus:ring-2 dark:border-neutral-800 dark:bg-neutral-900 dark:text-neutral-300 dark:placeholder-neutral-400",
+            className
+        )}
+    /> 
     )
-}
+})
+
